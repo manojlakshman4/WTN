4aefda2db1c26c7d93858fdb607fe61dee349249
Manoj <Manoj@192.168.0.4>
2020-06-12 13:26:21 +0530

    wipro Program 4a

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex4a.java
@@ -0,0 +1,16 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex4a {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		if(a>0) {
+			System.out.println("Positive");
+		}
+		else if(a<0) {
+			System.out.println("Negitive");
+		}
+		else {
+			System.out.println("Zero");
+		}
+	}
+}
