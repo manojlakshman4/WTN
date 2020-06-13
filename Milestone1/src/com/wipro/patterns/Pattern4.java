3af8357a6672b89514be1ebb0d8b6fb7cc2c9a77
Manoj <Manoj@192.168.0.4>
2020-06-13 12:47:35 +0530

    Wipro Program Patterns

---
+++ Milestone1/src/com/wipro/patterns/Pattern4.java
@@ -0,0 +1,13 @@
+package com.wipro.patterns;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(n-i+" ");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Milestone1/src/com/wipro/patterns/Pattern9.java
@@ -0,0 +1,13 @@
+package com.wipro.patterns;
+
+public class Pattern9 {
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		for(int i=0;i<n;i++) {
+			for(int j=i;j<n;j++) {
+				System.out.print(i+1+" ");
+			}
+			System.out.println();
+		}
+	}
+}
