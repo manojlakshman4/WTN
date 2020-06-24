d164e06b07585ef821bb7b82f8947a6c63aa95b8
Manoj <Manoj@192.168.0.4>
2020-06-24 16:48:47 +0530

    Classes and Objects Programs

--- Milestone1/src/oopsInheritance/classesAndObjects/BoxMain.java
+++ Milestone1/src/oopsInheritance/classesAndObjects/BoxMain.java
@@ -1,6 +1,6 @@
 package oopsInheritance.classesAndObjects;
 
-public class Ex1 {
+public class BoxMain {
 	public static void main(String[] args) {
 		Box b=new Box(10,20,10);
 		System.out.println(b.volume());
--- Milestone1/src/oopsInheritance/classesAndObjects/CalculatorMain.java
+++ Milestone1/src/oopsInheritance/classesAndObjects/CalculatorMain.java
@@ -1,6 +1,6 @@
 package oopsInheritance.classesAndObjects;
 
-public class Ex2 {
+public class CalculatorMain {
 	public static void main(String[] args) {
 		System.out.println(Calculator.powerInt(2, 3));
 		System.out.println(Calculator.powerDouble(2.5, 3));
